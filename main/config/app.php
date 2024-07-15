44c8b9dc331e8b8fbbea6295351a9231b988b33b
santergameboy <santer.gameboy@gmail.com>
2024-07-15 21:37:35 +0200

    Software1

---
+++ main/config/app.php
@@ -0,0 +1,32 @@
+<?php
+
+	const APP_URL="http://localhost/main/";
+	const APP_NAME="FUNDETAREA";
+	const APP_SESSION_NAME="POS";
+
+	/*----------  Tipos de documentos  ----------*/
+	const DOCUMENTOS_USUARIOS=["C.I.","DNI","Cedula","Licencia","Pasaporte","Otro"];
+
+
+	/*----------  Tipos de unidades de productos  ----------*/
+	const PRODUCTO_UNIDAD=["Unidad","Libra","Kilogramo","Caja","Paquete","Lata","Galon","Botella","Tira","Sobre","Bolsa","Saco","Tarjeta","Otro"];
+
+	/*----------  Configuración de moneda  ----------*/
+	const MONEDA_SIMBOLO="Bs";
+	const MONEDA_NOMBRE="Bolivianos";
+	const MONEDA_DECIMALES="2";
+	const MONEDA_SEPARADOR_MILLAR=",";
+	const MONEDA_SEPARADOR_DECIMAL=".";
+
+
+	/*----------  Marcador de campos obligatorios (Font Awesome) ----------*/
+	const CAMPO_OBLIGATORIO='&nbsp; <i class="fas fa-edit"></i> &nbsp;';
+
+	/*----------  Zona horaria  ----------*/
+	date_default_timezone_set("America/El_Salvador");
+
+	/*
+		Configuración de zona horaria de tu país, para más información visita
+		http://php.net/manual/es/function.date-default-timezone-set.php
+		http://php.net/manual/es/timezones.php
+	*/
---
+++ main/config/server.php
@@ -0,0 +1,6 @@
+<?php
+
+	const DB_SERVER="127.0.0.1";
+	const DB_NAME="ventas";
+	const DB_USER="root";
+	const DB_PASS='';
